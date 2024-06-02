2dda4caa975baf32f441d89e31e1972b278536ec
Phạm Duy Khánh <152389580+hoangnorpscripter@users.noreply.github.com>
2024-06-02 19:00:32 +0700

    Create memaybeo.lua

---
+++ memaybeo.lua
@@ -0,0 +1,5 @@
+if _G.Script == "Kaitun" then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/RafakjRoblox/Crack/main/Luxury%20%232"))()
+elseif _G.Script == "Main" then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/RafakjRoblox/Crack/main/Luxury%20%233"))()
+end
