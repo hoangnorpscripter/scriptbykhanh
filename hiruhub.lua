1c9cd182913db57f4b36c496946f536039527064
Phạm Duy Khánh <152389580+hoangnorpscripter@users.noreply.github.com>
2024-06-28 21:58:43 +0700

    Update hiruhub.lua

--- hiruhub.lua
+++ hiruhub.lua
@@ -3,8 +3,8 @@ local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/d
 local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
 ----------------------------------------------------------------------------------------------------------------------------------------------
 local Window = Fluent:CreateWindow({
-    Title = "Cat Hub",
-    SubTitle = "by: mèo béo thích macdonal",
+    Title = "Ditmemay hub",
+    SubTitle = "by: trumskidder",
     TabWidth = 160,
     Size = UDim2.fromOffset(500, 320),
     Acrylic = true,
